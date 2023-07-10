ec36f76c1f0beff716e42abda60141dd8a2b159e
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-07-10 11:48:52 +0800

    Create xray_uninstall.sh

---
+++ xray_uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+echo 正在清理已安装xray. . .
+
+systemctl disable xray
+
+systemctl stop xray
+
+rm -rf /usr/local/etc/xray
+
+rm -rf /usr/local/share/xray
+
+apt install wget unzip -y
+
+echo 正在下载配置文件. . .
+
+rm -f /etc/systemd/system/xray.service
+
+rm -f /etc/systemd/system/xray@.service
+
+rm -rf /etc/systemd/system/xray.service.d
+
+rm -rf /etc/systemd/system/xray@.service.d
+
+rm -f /etc/systemd/system/xray.service.d/10-donot_touch_single_conf.conf
+
+rm -f /etc/systemd/system/xray@.service.d/10-donot_touch_single_conf.conf
+
+systemctl daemon-reload
+
+echo Xray 卸载成功!
+
+exit 0
