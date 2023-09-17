2d0ab8d6fc269bab62b3f6f0c19a9476a94ce321
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-09-17 23:19:38 +0800

    Update Compile.sh

--- Compile.sh
+++ Compile.sh
@@ -72,18 +72,6 @@ echo '{}' > /usr/local/etc/xray/config.json
 
 systemctl daemon-reload
 
-NeedReinstall=$(xray --version)
-
-if [ ${NeedReinstall} = "This program can only be run on AMD64 processors with v3 microarchitecture support." ]; then
-
-  cd Xray-core && env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o xray -trimpath -ldflags "-s -w -buildid=" ./main
-  
-  mv -f xray /usr/local/bin/xray
-  
-  chmod +x /usr/local/bin/xray
-  
-fi
-
 systemctl enable xray
 
 systemctl start xray
