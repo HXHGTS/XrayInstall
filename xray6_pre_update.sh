db39132fced1917e7d08c03440504ee4d6097283
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2023-10-15 23:25:49 +0800

    Update xray6_pre_update.sh

--- xray6_pre_update.sh
+++ xray6_pre_update.sh
@@ -46,7 +46,7 @@ mv -f /var/tmp/xray/xray /usr/local/bin/xray
 
 chmod +x /usr/local/bin/xray
 
-wget -O /usr/local/share/xray/geoip.dat https://raw.githubusercontent.com/Loyalsoldier/geoip/release/geoip.dat
+wget -O /usr/local/share/xray/geoip.dat https://summer-poetry-7fa8.qq0mjpmkt9z.workers.dev/Loyalsoldier/geoip/releases/latest/download/geoip.dat
 
 wget -O /usr/local/share/xray/geosite.dat https://summer-poetry-7fa8.qq0mjpmkt9z.workers.dev/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
