b42ad19dcfc6b7a65efcf65eec229d7fb592dc71
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-07-14 14:05:27 +0800

    Update xray_update.sh

--- xray_update.sh
+++ xray_update.sh
@@ -1,55 +1,39 @@
 #!/bin/sh
 
 echo 正在关闭xray. . .
-
-systemctl disable xray
-
-systemctl stop xray
+if systemctl is-active --quiet xray; then
+    echo Xray正在运行，正在停止Xray. . .
+    systemctl disable xray --now
+else
+    echo Xray未运行.
+fi
 
 echo 正在进行更新前准备. . .
-
-mkdir -p /var/tmp/xray
-
-apt install wget unzip -y
+[ ! -d /var/tmp/xray ] && mkdir -p /var/tmp/xray
 
 echo 正在下载最新版Xray X64. . .
-
 wget https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -O /var/tmp/Xray-linux-64.zip
 
 echo 正在解压并安装. . .
-
 unzip -o -d /var/tmp/xray /var/tmp/Xray-linux-64.zip
 
 rm -f /var/tmp/xray/geoip.dat
-
 rm -f /var/tmp/xray/geosite.dat
 
 mv -f /var/tmp/xray/xray /usr/local/bin/xray
-
 chmod +x /usr/local/bin/xray
 
 wget -O /usr/local/share/xray/geoip.dat https://github.com/Loyalsoldier/geoip/releases/latest/download/geoip.dat
-
 wget -O /usr/local/share/xray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
-systemctl enable xray
-
-systemctl start xray
-
 echo 正在清理残余文件. . .
-
 rm -rf /var/tmp/xray
-
 rm -f /var/tmp/Xray-linux-64.zip
 
 echo Xray 更新成功!
 
 echo -------Xray版本号-------
-
 xray --version | grep Xray
-
-echo 
-
 echo ------------------------
 
 exit 0
