4a757721925c7120adf82f234c32f07826c210fb
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2024-07-14 14:02:09 +0800

    Update xray_uninstall.sh

--- xray_uninstall.sh
+++ xray_uninstall.sh
@@ -1,42 +1,24 @@
 #!/bin/sh
 
 echo 正在清理已安装xray. . .
+if systemctl is-active --quiet xray; then
+    echo Xray正在运行，正在停止Xray. . .
+    systemctl disable xray --now
+else
+    echo Xray未运行.
+fi
 
-systemctl disable xray
-
-systemctl stop xray
-
-rm -rf /usr/local/etc/xray
-
-rm -rf /usr/local/share/xray
-
-rm -f /usr/local/bin/xray
-
-rm -f /usr/local/bin/xray
-
-rm -rf /var/tmp/xray
-
-rm -rf /var/log/xray
-
-rm -f /var/tmp/Xray-linux-64.zip
-
-rm -f /var/tmp/GetXrayURL.txt
-
-rm -rf /root/Xray-core
+[ -d /usr/local/etc/xray ] && rm -rf /usr/local/etc/xray
+[ -d /usr/local/share/xray ] && rm -rf /usr/local/share/xray
+[ -f /usr/local/bin/xray ] && rm -f /usr/local/bin/xray
+[ -d /var/tmp/xray ] && rm -rf /var/tmp/xray
+[ -d /var/log/xray ] && rm -rf /var/log/xray
+[ -f /var/tmp/Xray-linux-64.zip ] && rm -f /var/tmp/Xray-linux-64.zip
+[ -f /var/tmp/GetXrayURL.txt ] && rm -f /var/tmp/GetXrayURL.txt
+[ -d /root/Xray-core ] && rm -rf /root/Xray-core
 
 echo 正在删除服务. . .
-
-rm -f /etc/systemd/system/xray.service
-
-rm -f /etc/systemd/system/xray@.service
-
-rm -rf /etc/systemd/system/xray.service.d
-
-rm -rf /etc/systemd/system/xray@.service.d
-
-rm -f /etc/systemd/system/xray.service.d/10-donot_touch_single_conf.conf
-
-rm -f /etc/systemd/system/xray@.service.d/10-donot_touch_single_conf.conf
+find /etc/systemd/system -name "*xray*" -exec rm -rf {} \;
 
 systemctl daemon-reload
 
